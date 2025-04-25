9a75463c4099ff5e0d3b6084e7cd94f45d3f7277
jEstrela <jaoestrella@hotmail.com>
2025-04-25 20:33:11 -0300

    erro?

--- src/app.py
+++ src/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Welcome to the Raspberry Pi web application! joao'
+  return 'Welcome to the Raspberry Pi web application! joao lindo!'
